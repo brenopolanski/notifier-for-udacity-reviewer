b38679cf0761d7dc797e64d11d9c2147cd6e752a
brenopolanski <breno.polanski@gmail.com>
2016-06-27 23:51:35 -0300

    Add fetch in API

---
+++ extension/api.js
@@ -0,0 +1,28 @@
+(function() {
+  'use strict';
+
+  var TOKEN = 'YOUR_TOKEN';
+
+  var BASE_URL = 'https://review-api.udacity.com/api/v1/me/';
+
+  var xhr = function() {
+    var xhr = new XMLHttpRequest();
+
+    return function(method, url, callback) {
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          callback(xhr.responseText);
+        }
+      };
+      xhr.open(method, url);
+      xhr.setRequestHeader('authorization', TOKEN);
+      xhr.send();
+    };
+  }();
+
+  window.udacityNotifyReviewer = function(callback) {
+    xhr('GET', BASE_URL + 'certifications.json', function(data) {
+      console.log(data);
+    });
+  };
+})();
--- extension/main.js
+++ extension/main.js
@@ -1,7 +1,14 @@
-/* globals chrome:true */
+/* globals chrome:true, udacityNotifyReviewer:true */
 
 (function() {
   'use strict';
+
+  function update(argument) {
+    udacityNotifyReviewer();
+  }
+
+  chrome.alarms.create({ periodInMinutes: 1 });
+  chrome.alarms.onAlarm.addListener(update);
 
   chrome.browserAction.onClicked.addListener(function() {
     chrome.tabs.create({
