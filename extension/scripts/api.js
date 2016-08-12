5e902fca231f4fe72f37e84b6dffcb0ca3d8ea66
brenopolanski <breno.polanski@gmail.com>
2016-08-11 22:35:51 -0300

    #7 - Switch from XHR to Fetch

--- extension/scripts/api.js
+++ extension/scripts/api.js
@@ -36,74 +36,79 @@
     return api;
   })();
 
-  const xhr = (() => {
-    const xhr = new XMLHttpRequest();
-    let token = window.Udacity.settings.get('oauthToken');
-
-    return (method, url, doneCallback, incompleteCallback) => {
-      token = window.Udacity.settings.get('oauthToken');
-
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          doneCallback(xhr.responseText);
-        }
-        else if (xhr.status >= 400) {
-          doneCallback(xhr.responseText);
-        }
-        else if (xhr.status >= 500) {
-          doneCallback(xhr.responseText);
-        }
-        else {
-          if (typeof incompleteCallback === 'function') {
-            incompleteCallback(xhr.responseText);
-          }
-        }
-      };
-      xhr.open(method, url);
-      xhr.setRequestHeader('Authorization', token);
-      xhr.send();
-    };
-  })();
-
   window.udacityNotifyReviewer = callback => {
     const rootUrl = window.Udacity.settings.get('rootUrl');
+    const token = window.Udacity.settings.get('oauthToken');
     const checkProject = window.Udacity.settings.get('checkProject');
+    const headers = new Headers({
+      'Authorization': token,
+      'Content-Length': '0'
+    });
     let languages = window.Udacity.settings.get('languages');
 
     if (typeof languages === 'string') {
       languages = languages.split(',');
     }
 
-    xhr('GET', `${rootUrl}me/certifications.json`, data => {
-      const certifications = JSON.parse(data);
-      let reviewAwaiting = 0;
+    fetch(`${rootUrl}me/certifications.json`, {
+      method: 'GET',
+      headers: headers
+    })
+    .then(response => {
+      const status = response.status;
+      const linkHeader = response.headers.get('Link');
 
-      if (!certifications.hasOwnProperty('error')) {
-        certifications.map(certification => {
-          // Check if is certified (required to review projects)
-          if (certification.certified_at) {
-            const languageReviewsCount = certification.project.awaiting_review_count_by_language;
-            const projectId = certification.project.id;
+      if (linkHeader === null) {
+        return response.json().then(data => {
+          const certifications = data;
+          let reviewAwaiting = 0;
 
-            if (languageReviewsCount) {
-              languages.map(language => {
-                if (language in languageReviewsCount) {
-                  if (checkProject) {
-                    xhr('POST', `${rootUrl}projects/${projectId}/submissions/assign.json`);
-                  }
+          if (!certifications.hasOwnProperty('error')) {
+            certifications.map(certification => {
+              // Check if is certified (required to review projects)
+              if (certification.certified_at) {
+                const languageReviewsCount = certification.project.awaiting_review_count_by_language;
+                const projectId = certification.project.id;
 
-                  reviewAwaiting += languageReviewsCount[language];
+                if (languageReviewsCount) {
+                  languages.map(language => {
+                    if (language in languageReviewsCount) {
+                      if (checkProject) {
+                        fetch(`${rootUrl}projects/${projectId}/submissions/assign.json`, {
+                          method: 'POST',
+                          headers: headers
+                        });
+                      }
+
+                      reviewAwaiting += languageReviewsCount[language];
+                    }
+                  });
                 }
-              });
-            }
+              }
+            });
+
+            callback(reviewAwaiting);
+          }
+          else {
+            callback(certifications.error);
           }
         });
+      }
 
-        callback(reviewAwaiting);
+      if (status >= 400) {
+        return response.json().then(res => {
+          callback(res.error);
+        });
       }
-      else {
-        callback(certifications.error);
+
+      if (status >= 500) {
+        return response.json().then(res => {
+          callback(res.error);
+        });
       }
+    })
+    .catch(err => {
+      console.error('Failed retrieving information', err);
     });
   };
 })();
